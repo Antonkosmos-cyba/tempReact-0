6eb931077f9d3e7b5c9da9ca562432396da478c0
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-04-28 13:24:54 +0300

    /

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { CustLink } from "../Customlink";
-import logo from "./rings1.png";
+import logo from "./logo-1.png";
 
 function Heder() {
   return (
