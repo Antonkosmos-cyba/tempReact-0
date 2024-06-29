fec8e90f609d4fcaff945be80001c04c1a31a3f7
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-06-29 13:53:45 +0300

    foto

--- src/components/pages/vaina/Vaina.jsx
+++ src/components/pages/vaina/Vaina.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./vaina.css";
 
 import Carousel from "react-bootstrap/Carousel";
-import Image1 from "./vaina4.png";
-import Image2 from "./vn2.png";
-import Image3 from "./vn3.png";
+import Image1 from "./ak2.jpg";
+import Image2 from "./ak3.jpeg";
+import Image3 from "./ak4.jpg";
 
 function Vaina() {
   return (
