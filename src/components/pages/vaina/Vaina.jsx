4684f9836a7f764418465bca1c51cbcba9d5f410
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-07-02 14:06:58 +0300

    foto nove

--- src/components/pages/vaina/Vaina.jsx
+++ src/components/pages/vaina/Vaina.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import "./vaina.css";
 
 import Carousel from "react-bootstrap/Carousel";
-import Image1 from "./1ak.jpg";
-import Image2 from "./2ak.jpg";
-import Image3 from "./3ak.jpg";
-import Image4 from "./4ak.jpg";
-import Image5 from "./5ak.jpg";
-import Image6 from "./6ak.jpg";
+// import Image1 from "./1ak.jpg";
+import Image2 from "./2akn.jpg";
+import Image3 from "./3akn.jpg";
+import Image4 from "./4akn.jpg";
+import Image5 from "./5akn.jpg";
+import Image6 from "./6akn.jpg";
 
 function Vaina() {
   return (
     <div className="cont">
       <Carousel fade className="positem">
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <img src={Image1} alt="" className="pos"></img>
-        </Carousel.Item>
+        </Carousel.Item> */}
 
         <Carousel.Item>
           <img src={Image2} alt="" className="pos"></img>
