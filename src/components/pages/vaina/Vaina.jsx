5d9671bcd947995340d74ed5243c1586901446bd
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-07-03 16:02:31 +0300

    vain

---
+++ src/components/pages/vaina/Vaina.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./vaina.css";
+
+import Carousel from "react-bootstrap/Carousel";
+// import Image1 from "./1ak.jpg";
+import Image2 from "./2ak2.jpg";
+import Image3 from "./3ak3.jpg";
+import Image4 from "./4ak4.jpg";
+import Image5 from "./5ak5.jpg";
+import Image6 from "./6ak6.jpg";
+
+function Vaina() {
+  return (
+    <div className="cont">
+      <Carousel fade className="positem">
+        {/* <Carousel.Item>
+          <img src={Image1} alt="" className="pos"></img>
+        </Carousel.Item> */}
+
+        <Carousel.Item>
+          <img src={Image2} alt="" className="pos"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Image3} alt="" className="pos"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Image4} alt="" className="pos"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Image5} alt="" className="pos"></img>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Image6} alt="" className="pos"></img>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Vaina;
